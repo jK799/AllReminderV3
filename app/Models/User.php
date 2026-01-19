3dbdba3d94c7546659258c5ca1f076b7aa228730
Jakub <j.hruby79@gmail.com>
2026-01-19 17:00:25 +0100

    feat(models): add user roles relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Role;
+
 
 class User extends Authenticatable
 {
@@ -45,4 +48,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function roles(): BelongsToMany
+{
+    return $this->belongsToMany(Role::class);
+}
+
 }
