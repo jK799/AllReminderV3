c93bcfd4ba2126de6680c65777e3e6b7467b544d
Jakub <j.hruby79@gmail.com>
2026-01-19 16:39:23 +0100

    feat(db): add devices table

---
+++ database/migrations/2026_01_19_153802_create_devices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        
+            $table->string('name');
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->string('serial_number')->nullable();
+        
+            $table->date('purchase_date')->nullable();
+            $table->text('notes')->nullable();
+        
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
