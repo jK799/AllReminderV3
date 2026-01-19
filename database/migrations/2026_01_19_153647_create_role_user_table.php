43e2d068fbabaa8773afabb24d912ff22d23332f
Jakub <j.hruby79@gmail.com>
2026-01-19 17:02:46 +0100

    fix(db): correct role_user pivot columns

--- database/migrations/2026_01_19_153647_create_role_user_table.php
+++ database/migrations/2026_01_19_153647_create_role_user_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+        
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+        
             $table->timestamps();
+        
+            $table->unique(['user_id', 'role_id']);
         });
+        
     }
 
     /**
