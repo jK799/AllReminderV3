c0372038a7fe4c9ed968b4ddd4a90f1744d10432
Jakub <j.hruby79@gmail.com>
2026-01-21 18:13:16 +0100

    feat(api): implement devices CRUD with ownership checks

---
+++ app/Http/Controllers/Api/DeviceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreDeviceRequest;
+use App\Http\Requests\UpdateDeviceRequest;
+use App\Http\Resources\DeviceResource;
+use App\Models\Device;
+use Illuminate\Http\Response;
+
+class DeviceController extends Controller
+{
+    public function index()
+    {
+        $devices = Device::query()
+            ->where('user_id', auth()->id())
+            ->latest()
+            ->get();
+
+        return DeviceResource::collection($devices);
+    }
+
+    public function store(StoreDeviceRequest $request)
+    {
+        $device = Device::create([
+            'user_id' => auth()->id(),
+            ...$request->validated(),
+        ]);
+
+        return (new DeviceResource($device))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function show(Device $device)
+    {
+        $this->ensureOwner($device);
+
+        return new DeviceResource($device);
+    }
+
+    public function update(UpdateDeviceRequest $request, Device $device)
+    {
+        $this->ensureOwner($device);
+
+        $device->update($request->validated());
+
+        return new DeviceResource($device);
+    }
+
+    public function destroy(Device $device)
+    {
+        $this->ensureOwner($device);
+
+        $device->delete();
+
+        return response()->noContent();
+    }
+
+    private function ensureOwner(Device $device): void
+    {
+        abort_if($device->user_id !== auth()->id(), Response::HTTP_FORBIDDEN);
+    }
+}
