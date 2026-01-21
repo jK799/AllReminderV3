2e18a88a5b9b871cb49a18ed84ccdb2e55eb0673
Jakub <j.hruby79@gmail.com>
2026-01-21 23:12:23 +0100

    frontend: add axios api client with token storage

---
+++ resources/js/services/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/",
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export function setToken(token) {
+  if (token) localStorage.setItem("token", token);
+  else localStorage.removeItem("token");
+}
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export default api;
